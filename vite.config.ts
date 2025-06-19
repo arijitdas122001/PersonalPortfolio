9064229f3b198a1caf332ae2c731d1bd73282a46
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2025-06-20 00:01:16 +0530

    fix:vite.config.json

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "../attached_assets"),
     },
   },
+  root:path.resolve(__dirname),
   build: {
     outDir: "dist",
     emptyOutDir: true,
